5d101e9e28a5de4f6a00a323d29c3f28bb690015
Baronno <falconhui8@gmail.com>
2021-06-26 09:55:25 +1000

    adding header components and links to homw, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './pages/homepage.component'
 import { Switch, Route } from 'react-router-dom'
 
 import ShopPage from './pages/shop/shop.component.jsx'
-
+import Header from './components/header/header.component'
 /* exact disables the /，保护后面的route Switch make sure we only
 render the router that matched */
 /* link force to render rebuld the web application, but the route deoesn;t,
@@ -14,6 +14,7 @@ it only determine which js component to render,  */
 function App () {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
